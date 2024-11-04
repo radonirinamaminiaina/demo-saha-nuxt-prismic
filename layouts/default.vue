eab6a35b33e535576cc50e813fa7da5c32ea50cf
Radonirina Maminiaina <rado93devweb@gmail.com>
2024-11-04 15:47:59 +0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
     <nav class="container-section flex gap-4 h-14 items-center">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/blog">Blog</NuxtLink>
+      <a href="https://saha-technology.com/blog">SAHA Technology Blog</a>
     </nav>
   </header>
   <main class="container-section">
